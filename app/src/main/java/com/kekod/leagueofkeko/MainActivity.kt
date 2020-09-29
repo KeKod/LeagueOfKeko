257ecde199e151d696559164cffc1020b3f53a33
Bir MasterKeko Hatirasi <sezginzgurr@gmail.com>
2020-09-29 22:45:23 +0300

    #Minik oyunlar

--- app/src/main/java/com/kekod/leagueofkeko/MainActivity.kt
+++ app/src/main/java/com/kekod/leagueofkeko/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_page_masterkeko)
 
 /*        if (BuildConfig.isFreemium) {
             btnMatchHistory.visibility = View.GONE
