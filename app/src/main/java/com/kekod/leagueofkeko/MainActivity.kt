d4abccd972ab6ae7e1a050ee2c5a1d37e2c2c9f0
Bir MasterKeko Hatirasi <sezginzgurr@gmail.com>
2020-09-29 22:38:53 +0300

    #finished Login Page

--- app/src/main/java/com/kekod/leagueofkeko/MainActivity.kt
+++ app/src/main/java/com/kekod/leagueofkeko/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.kekod.leagueofkeko
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (BuildConfig.isFreemium) {
+/*        if (BuildConfig.isFreemium) {
             btnMatchHistory.visibility = View.GONE
-        }
+        }*/
     }
 }
